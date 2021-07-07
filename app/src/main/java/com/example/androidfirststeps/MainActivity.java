07c0ffcafb141201cf2632a5160c1ae88e9788ba
ElenaGaricheva <elenagaricheva95@gmail.com>
2021-07-07 10:54:55 +0300

    Lesson1.Task1

--- app/src/main/java/com/example/androidfirststeps/MainActivity.java
+++ app/src/main/java/com/example/androidfirststeps/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.androidfirststeps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test_activity);
+    }
+
+    public void onToggleClicked(View view) {
+    }
+
+    public void onCheckboxClicked(View view) {
+    }
+
+    public void sendMessage(View view) {
     }
 }
